ea77891bd98ef6dbf42f6cd21388c40f3cd98558
Lahiru Tennakoon <lahiru.tennakoon@my.sliit.lk>
2020-06-07 23:10:08 +0530

    Passing method references between components.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 29 },
+      { name: "Stephanie", age: 26 },
+    ],
+    otherState: "some other value",
+  };
+
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: "Manu", age: 29 },
+        { name: "Stephanie", age: 27 },
+      ],
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1> Hi, I'm a React app </h1>
+        <p> This is really working!</p>
+        {/*Inefficient*/}
+        <button onClick={() => this.switchNameHandler("Maximilian!!")}>Switch Name</button>
+
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}/>
+        {/*Efficient*/}
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, "Max!")}> My hobbies: Racing</Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}/>
+      </div>
+    );
+  }
 }
 
 export default App;
