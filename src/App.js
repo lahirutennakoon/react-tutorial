0d309cdabdc74c1e601c8acf2ba2e2b13fbafbaa
Lahiru Tennakoon <lahiru@speralabs.com>
2020-06-14 22:24:52 +0530

    2-way binding. Styling with css.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
+
+/*
+// React hooks
+const App = (props) => {
+
+    const [personsState, setPersonsState] = useState({
+        persons: [
+            { name: "Max", age: 28 },
+            { name: "Manu", age: 29 },
+            { name: "Stephanie", age: 26 },
+        ],
+    });
+
+    const [otherState, setOtherState] = useState("some other value");
+
+    const switchNameHandler = (newName) => {
+        setPersonsState({
+            persons: [
+                { name: newName, age: 28 },
+                { name: "Manu", age: 29 },
+                { name: "Stephanie", age: 27 },
+            ],
+        })
+    };
+
+    return (
+      <div className="App">
+        <h1> Hi, I'm a React app </h1>
+        <p> This is really working!</p>
+        {/!*Inefficient*!/}
+        <button onClick={() => switchNameHandler("Maximilian!!")}>Switch Name</button>
+
+        <Person
+          name={personsState.persons[0].name}
+          age={personsState.persons[0].age}/>
+        {/!*Efficient*!/}
+        <Person
+          name={personsState.persons[1].name}
+          age={personsState.persons[1].age}
+          click={switchNameHandler.bind(this, "Max!")}> My hobbies: Racing</Person>
+        <Person
+          name={personsState.persons[2].name}
+          age={personsState.persons[2].age}/>
+      </div>
+    );
+}*/
 
 class App extends Component{
   state = {
@@ -22,13 +68,32 @@ class App extends Component{
     })
   }
 
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: "Max", age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: "Stephanie", age: 26 },
+      ],
+    })
+  }
+
   render() {
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer",
+    }
     return (
       <div className="App">
         <h1> Hi, I'm a React app </h1>
         <p> This is really working!</p>
         {/*Inefficient*/}
-        <button onClick={() => this.switchNameHandler("Maximilian!!")}>Switch Name</button>
+        <button
+          style={style}
+          onClick={() => this.switchNameHandler("Maximilian!!")}>Switch Name</button>
 
         <Person
           name={this.state.persons[0].name}
@@ -37,7 +102,8 @@ class App extends Component{
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, "Max!")}> My hobbies: Racing</Person>
+          click={this.switchNameHandler.bind(this, "Max!")}
+          changed={this.nameChangedHandler}> My hobbies: Racing</Person>
         <Person
           name={this.state.persons[2].name}
           age={this.state.persons[2].age}/>
