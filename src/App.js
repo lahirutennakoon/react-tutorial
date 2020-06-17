2d1bbb0c01bed6c8e32e1978f5af5b36b0602811
Lahiru Tennakoon <lahiru@speralabs.com>
2020-06-17 20:54:27 +0530

    Flexible lists with keys.

--- src/App.js
+++ src/App.js
@@ -51,9 +51,9 @@ const App = (props) => {
 class App extends Component{
   state = {
     persons: [
-      { name: "Max", age: 28 },
-      { name: "Manu", age: 29 },
-      { name: "Stephanie", age: 26 },
+      { id: "1", name: "Max", age: 28 },
+      { id: "2", name: "Manu", age: 29 },
+      { id: "3", name: "Stephanie", age: 26 },
     ],
     otherState: "some other value",
     showPersons: false,
@@ -67,13 +67,26 @@ class App extends Component{
     });
   };
 
-  nameChangedHandler = (event) => {
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((p) => {
+      return p.id === id;
+    });
+
+    // Using spread operator (ES6 syntax)
+    const person = {
+      ...this.state.persons[personIndex],
+    };
+
+    // Before ES6
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
     this.setState({
-      persons: [
-        { name: "Max", age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: "Stephanie", age: 26 },
-      ],
+      persons: persons,
     })
   }
 
@@ -100,9 +113,11 @@ class App extends Component{
         <div>
           {this.state.persons.map((person, index) => {
             return <Person
+              key={person.id}
               name={person.name}
               age={person.age}
-            click={() => this.deletePersonsHandler(index)} />
+              changed={(event) => this.nameChangedHandler(event, person.id)}
+              click={() => this.deletePersonsHandler(index)} />
           })}
         </div>
       );
